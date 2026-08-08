b6a73678f8561c5be757176da84a424522797dd2
Your Full Name <hspatil000@gmail.com>
2026-08-08 12:29:58 +0530

    FS module with sync async and promises

---
+++ fs-async.js
@@ -0,0 +1,16 @@
+import fs from "node:fs"
+
+// fs.writeFile("README.md","This is the FS module",(err)=>{
+//     console.log(err);
+// });
+
+
+// fs.readFile("README.md","utf-8",(data,err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log(data)
+
+//     }
+// })
---
+++ fs-sync.js
@@ -0,0 +1,18 @@
+import fs from 'node:fs'
+
+// //write
+// fs.writeFileSync("test.txt","Yo yo honey singh");
+
+// //read
+// const data = fs.readFileSync("test.txt", "utf-8")
+// console.log(data);
+
+
+// //adding text in file -L:
+// fs.appendFileSync("test.txt", "\nHello JI");
+
+//creating folder
+// fs.mkdirSync("myfolder");
+
+//delete files
+fs.rmdirSync("myfolder")
